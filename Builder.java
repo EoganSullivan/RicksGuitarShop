59c308de12abdbc462357b275f366252fec65ef4
Eogan Sullivan <eogan.sullivan@outlook.com>
2016-10-17 13:58:02 +0100

    Inital Commit
    Inital Commit of Ricks Guitar

---
+++ Builder.java
@@ -0,0 +1,8 @@
+/**
+ * Created by t00166087 on 26/09/2016.
+ */
+public enum Builder{
+    GIBSON, FENDER, PRS, RYAN, COLLINGS, OLSON, MARTIN
+}
+
+
---
+++ Type.java
@@ -0,0 +1,6 @@
+/**
+ * Created by t00166087 on 26/09/2016.
+ */
+public enum Type {
+    ELECTRIC, ACOUSTIC
+}
